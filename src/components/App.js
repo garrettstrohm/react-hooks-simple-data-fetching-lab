ce704962a6bdd79338df9c909714660ec49f54bf
Garrett <garres4@vt.edu>
2021-11-08 17:30:29 -0700

    commit tree d9ac6b8e9e8f33e0e8ecfe044b069b213733855f

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import{useEffect, useState} from 'react'
+
+function App() {
+    const [dogPic, setDogPic] = useState("")
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(r => r.json())
+        .then(data => {
+            setDogPic(data.message)
+            setIsLoaded(true)
+        })
+    }, [])
+
+    if(!isLoaded) {
+        return <p>Loading...</p>
+    }
+
+    return(
+        <img src={dogPic} alt='A Random Dog'/>
+    );
+}
+
+export default App
